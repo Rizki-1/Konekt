2aaa6e164f560a4c634a97e40cbe4d00b7838168
aisyeee <aisyeee2006@gmail.com>
2023-09-20 09:15:43 +0700

    hapus all message

--- resources/views/DashboardUser/menu.blade.php
+++ resources/views/DashboardUser/menu.blade.php
@@ -339,7 +339,6 @@
                                         class="img-fluid avatar avatar-50 avatar-rounded">
                                     <div class="caption ms-3 d-none d-md-block ">
                                         <h6 class="mb-0 caption-title">{{ Auth::user()->name }}</h6>
-
                                     </div>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">

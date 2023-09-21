d9c0f35557f049ef7687dfd9d1075f6614ff9321
rayya17 <rayyakhnsa@gmail.com>
2023-09-21 08:20:59 +0700

    pesanan

--- resources/views/DashboardUser/pesanan.blade.php
+++ resources/views/DashboardUser/pesanan.blade.php
@@ -45,11 +45,6 @@
     margin-left: 15px;
     margin-top: 15px;
   }
-  .tgl{
-    color: rgb(140, 112, 112);
-    margin-left: 200px;
-
-  }
   .main-content {
     flex: 2px;
   }
@@ -69,47 +64,6 @@
   <body class="  "  style="background:url(../../assets/images/dashboard.png);    background-attachment: fixed;
     background-size: cover;">
     @include('layout.logoloader')
-    @foreach ($user as $p)
-    <form action="{{ route('pengembaliandana', ['id' => $p->id]) }}" method="POST">
-        @csrf
-    <div class="modal" id="myModal-{{$p->id}}" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Tambah Pesanan</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-4">
-                                <img src="{{ asset('storage/' . $p->fotomakanan) }}" alt="Foto Menu" class="img-fluid">
-                            </div>
-                            <div class="col-8">
-                                <p class="fs-6 text-primary">
-                                    Harga :
-                                    Rp. {{ $p->totalharga}}
-                                    <input type="hidden" id="harga-{{$p->id}}" name="harga" value="{{ $p->id }}">
-                                    <input type="hidden" id="totalHarga-{{$p->id}}" name="totalHarga" value="">
-                                    <input type="hidden" id="" name="pengembaliandana_id" value="{{ $p->id }}">
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="jumlah-{{$p->id}}" class="form-label fw-bold">Jumlah</label>
-                        <input type="number" id="jumlah-{{$p->id}}" name="jumlah" class="form-control" placeholder="Masukan jumlah">
-                    </div>
-                    </div>
-                <div class="modal-footer">
-
-                    <button type="submit" class="btn btn-warning">ajukan pengembalian dana</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
-@endforeach
     <aside class="sidebar sidebar-default sidebar-hover sidebar-mini navs-pill-all ">
         <div class="sidebar-header d-flex align-items-center justify-content-start">
             @include('layout.minilogo')
@@ -299,14 +253,44 @@
                 </li>
                 <!-- End notifikasi -->
                 <!-- start pesan -->
-                
+                <li class="nav-item dropdown">
+                  <a href="#" class="nav-link" id="mail-drop2" data-bs-toggle="dropdown"  aria-haspopup="true" aria-expanded="false">
+                    <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="0.4" d="M20 13.441C20 16.231 17.76 18.491 14.97 18.501H14.96H5.05C2.27 18.501 0 16.251 0 13.461V13.451C0 13.451 0.006 9.02498 0.014 6.79898C0.015 6.38098 0.495 6.14698 0.822 6.40698C3.198 8.29198 7.447 11.729 7.5 11.774C8.21 12.343 9.11 12.664 10.03 12.664C10.95 12.664 11.85 12.343 12.56 11.763C12.613 11.728 16.767 8.39398 19.179 6.47798C19.507 6.21698 19.989 6.45098 19.99 6.86798C20 9.07698 20 13.441 20 13.441Z" fill="currentColor"/>
+                    <path d="M19.4769 3.174C18.6109 1.542 16.9069 0.5 15.0309 0.5H5.05086C3.17486 0.5 1.47086 1.542 0.60486 3.174C0.41086 3.539 0.50286 3.994 0.82586 4.252L8.25086 10.191C8.77086 10.611 9.40086 10.82 10.0309 10.82C10.0349 10.82 10.0379 10.82 10.0409 10.82C10.0439 10.82 10.0479 10.82 10.0509 10.82C10.6809 10.82 11.3109 10.611 11.8309 10.191L19.2559 4.252C19.5789 3.994 19.6709 3.539 19.4769 3.174Z" fill="currentColor"/>
+                    </svg>
+                    <span class="bg-primary count-mail"></span>
+                  </a>
+                  <div class="sub-drop dropdown-menu dropdown-menu-end p-0" aria-labelledby="mail-drop2">
+                      <div class="card shadow-none m-0">
+                        <div class="card-header d-flex justify-content-between bg-primary mx-0 px-4">
+                            <div class="header-title">
+                              <h5 class="mb-0 text-white">All Message</h5>
+                            </div>
+                        </div>
+                        <div class="card-body p-0 ">
+                            <a href="#" class="iq-sub-card">
+                              <div class="d-flex  align-items-center">
+                                  <div class="">
+                                    <img class="avatar-40 rounded-pill" src="../assets/images/layouts/01.png" alt="">
+                                  </div>
+                                  <div class="ms-3">
+                                    <h6 class="mb-0 ">jbdh</h6>
+                                    <small class="float-start font-size-12">13 Jun</small>
+                                  </div>
+                              </div>
+                            </a>
+                        </div>
+                      </div>
+                  </div>
+                </li>
                 <!-- End Pesan-->
                 <!-- Start Profile-->
                 <li class="nav-item dropdown">
                   <a class="nav-link py-0 d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="../assets/images/avatars/01.png" alt="User-Profile" class="img-fluid avatar avatar-50 avatar-rounded">
                     <div class="caption ms-3 d-none d-md-block ">
-                        <h6 class="mb-0 caption-title">{{ Auth::user()->name }}</h6>
+                        <h6 class="mb-0 caption-title">Austin Robertson</h6>
                         <p class="mb-0 caption-sub-title">Marketing Administrator</p>
                     </div>
                   </a>
@@ -344,57 +328,80 @@
       style="position: relative"
       >
       <center><h3>Kuliner kita <span style="color: #EA6A12">|pesanan</span></h3></center>
-      @foreach ($user as $u)
+      <div class="mt-3">
+    <div class="row row-cols-2">
+        @foreach ($user as $u)
+        <div class="col mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{$u->toko_id}}</h5>
+                    <hr style="border-top: 2px solid #EA6A12; margin-top: 10px 0;">
 
-      <div class="container">
-          <div class="card d-flex">
-              <div class="">
-                  <div class="card-content d-flex">
-                      <img src="{{ asset('css/img/cafe.jpeg') }}" class="foto" alt="">
-                      <div class="img-content">
-                          <a class="tgl">{{ $u->created_at }}</a>
-                          <h5 class="mt-12">Masakan nasi - warung berkah</h5>
-                          <tr>
-                              <h6>Nasi Goreng</h6>
-                          </tr><br>
-                          <tr>
-                              <h6>No antrian {{ $u->nomor_antrian }}</h6>
-                          </tr>
-                          <tr>
-                              <h6>Rp.{{ $u->totalharga }} 1 (menu) {{ $u->metodepembayaran }}</h6>
-                          </tr><br>
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <img src="{{ asset('css/img/cafe.jpeg') }}" alt="Gambar Menu" width="100">
+                        </div>
+                        <div>
+                            <a>
+                                <small class="card-text">{{$u->created_at}}</small>
+                            </a>
+                        </div>
+                    </div>
 
-                          @if ($u->pembelianstatus === 'selesai')
-                              <form action="{{ route('tandakanselesai',['id' => $u->id]) }}" method="POST">
-                                  @csrf
-                                  @method('PATCH')
-                                  <button class="btn btn-primary" type="submit">Tandakan telahselesai</button>
-                              </form>
-                          @elseif ($u->pembelianstatus === 'pesanan di tolak')
-                              <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal-{{ $u->id }}" type="submit">Ajukan pengembalian dana</button>
-                          @elseif ($u->pembelianstatus === 'menunggu konfirmasi' )
-                              <div class="btn btn-danger" style="float:right"><span class="color:#EA6A12">Batalkan pesanan</span></div>
-                          @elseif ($u->pembelianstatus === 'sedang di proses')
-                          <div class="btn btn-warning" style="float:right"><span class="color:#EA6A12">Sedang di proses</span></div>
-                          @else
-                          @endif
+                    <hr style="border-top: 1px solid #d3d3d3; margin: 10px 0;">
 
-                          @foreach ($pengembaliandana as $p)
-                              @if ($p->pengembalian_status === 'pengajuan sedang di proses' && $u->pembelianstatus === 'mengajukan pengembalian')
-                                  <button>{{ $p->pengembalian_status }}</button>
-                              @endif
-                          @endforeach
+                    <div class="d-flex justify-content-between">
+                        <a class="card-text">No Antrian :
+                        <tr>{{$u->nomor_antrian}}</tr>
+                        </a>
+                    </div>
 
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+                    <hr style="border-top: 1px solid #d3d3d3; margin: 5px 0;">
 
-  @endforeach
+                    <div class="d-flex justify-content-between">
+                        <h6 class="card-text">Harga :
+                          <tr>Rp. {{ number_format($u->totalharga) }}</tr>
+                        </h6>        
+                    </div>
 
+                    <hr style="border-top: 1px solid #d3d3d3; margin: 5px 0;">
+
+                    <div class="d-flex justify-content-between">
+                        <h6 class="card-text">Pembayaran :
+                        <tr>{{$u->metodepembayaran}}</tr>
+                        </h6>
+                    </div>
+
+                    <hr style="border-top: 1px solid #d3d3d3; margin: 5px 0;">
+
+                    <div>
+                        @if ($u->pembelianstatus === 'selesai')
+                            <form action="{{ route('tandakanselesai',['id' => $u->id]) }}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button class="btn btn-primary" type="submit">Tandakan telah selesai</button>
+                            </form>
+                        @elseif ($u->pembelianstatus === 'pesanan di tolak')
+                            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal-{{ $u->id }}" type="submit">Ajukan pengembalian dana</button>
+                        @elseif ($u->pembelianstatus === 'menunggu konfirmasi' )
+                            <div class="btn btn-danger" style="float:right"><span class="color:#EA6A12">Batalkan pesanan</span></div>
+                        @elseif ($u->pembelianstatus === 'sedang di proses')
+                            <div class="btn btn-warning" style="float:right"><span class="color:#EA6A12">Sedang di proses</span></div>
+                        @endif
+
+                        @foreach ($pengembaliandana as $p)
+                            @if ($p->pengembalian_status === 'pengajuan sedang di proses' && $u->pembelianstatus === 'mengajukan pengembalian')
+                                <button>{{ $p->pengembalian_status }}</button>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
-      </div>
+
       {{-- @include('layout.footer') --}}
   </main>
   @include('layout.js')

8082a0f43abbd7783e443ed3d8656bf4c96ea08f
Rizki-1 <akunrizky85@gmail.com>
2023-10-09 09:07:20 +0700

    tambahan lagi

--- database/migrations/2023_11_13_035729_create_pengajuandanapenjuals_table.php
+++ database/migrations/2023_11_13_035729_create_pengajuandanapenjuals_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('pengajuandanapenjuals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('penjual_id')->constrained('penjuallogins')->onUpdate('cascade')->nullable()->default(null);
+            $table->foreignId('penjual_id')->nullable()->constrained('penjuallogins')->onUpdate('cascade');
             $table->foreignId('userOrder_id')->constrained('user_Orders')->onUpdate('cascade');
-            $table->foreignId('metodepembayaran_id')->constrained('pembayaranpenjuals')->onUpdate('cascade')->nullable()->default(null);
+            $table->foreignId('metodepembayaran_id')->nullable()->constrained('pembayaranpenjuals')->onUpdate('cascade');
             $table->string('keterangan_pengajuan')->nullable();
             $table->string('tujuan_pengajuan')->nullable();
             $table->string('status');
